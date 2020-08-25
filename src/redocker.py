2e82e230a07c563be00973ce892ad3f787d7653c
KvitnucaZahradka <martin.polacek@stonybrook.edu>
2020-08-24 22:16:07 -0400

    the new redocker started

--- src/redocker.py
+++ src/redocker.py
@@ -6,3 +6,18 @@ Created on 2020 June 01 23:42:50 (EST)
 
 @author: KanExtension
 """
+
+import argparse
+import os
+import yaml
+import importlib
+
+## TODO: create the README.md generator
+
+try:
+    import redocker_utils as uu
+except:
+    from . import redocker_utils as uu
+
+importlib.reload(uu)
+
